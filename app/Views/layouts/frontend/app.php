5d274cb7a2de9de2674378ddf5e44cc3f00a50ff
rahmathidayat9 <yeageirstsatu@gmail.com>
2021-02-15 14:17:21 +0700

    Done

--- app/Views/layouts/frontend/app.php
+++ app/Views/layouts/frontend/app.php
@@ -34,32 +34,7 @@
 <body id="page-top">
 
   <!-- ======= Header/ Navbar ======= -->
-  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll" href="#page-top"><?= setHeader()->navbar_title ?></a>
-      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link js-scroll active" href="/home">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll" href="#work">Portfolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll" href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?= $this->include('layouts/frontend/navbar'); ?>
 
   <!-- ======= Intro Section ======= -->
   <div id="home" class="intro route bg-image" style="background-image: url(<?= base_url('uploads/headers/'.setHeader()->image); ?>)">
---
+++ app/Views/layouts/frontend/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+  <div class="container">
+    <a class="navbar-brand js-scroll" href="#page-top"><?= setHeader()->navbar_title ?></a>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll active" href="/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll" href="#work">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
